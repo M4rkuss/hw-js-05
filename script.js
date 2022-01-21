48b26b6a39b45d539d4033f15b894950ff3c5e9c
m4rkuss <markbu07@gmail.com>
2022-01-21 12:11:16 +0200

    [add] the last version

--- script.js
+++ script.js
@@ -9,17 +9,17 @@
 // }
 // console.log(sum)
 
-// // 2) Наибольший общий делитель 2ух чисел ДОДЕЛАТЬ
-// let num1 = +prompt('Введите первое меньшее число')
-// let num2 = +prompt('Введите второе большее число')
-// let gcd = 0
-//
-// for (let i = 1; i <= num1; i++) {
-//   if (num1 % i === 0 && num2 % i === 0){
-//     gcd = i
-//   }
-//   console.log(gcd)
-// }
+// 2) Наибольший общий делитель 2ух чисел ДОДЕЛАТЬ
+let num1 = +prompt('Введите первое меньшее число')
+let num2 = +prompt('Введите второе большее число')
+let gcd = 1
+
+for (let i = 1; i <= num1; i++) {
+  if (num1 % i === 0 && num2 % i === 0 && i > gcd){
+    gcd = i
+  }
+}
+console.log(gcd)
 
 // // 3) Вывести делители числа
 // let num = +prompt('Введите число')
@@ -36,4 +36,28 @@
 // size = num.split("")
 // console.log('В вашем числе', size.length ,'цифр')
 
-// 5) Длинное задание подсчитать цифры
+// // 5) Длинное задание подсчитать цифры
+// let userNumbers = prompt('Введите 10 чисел через пробел')
+// let arrayOfNumbers = userNumbers.split(' ')
+// let positive = 0
+// let negative = 0
+// let zero = 0
+// let even = 0
+// let odd = 0
+//
+// arrayOfNumbers.forEach((value, idx, array) => {
+//   console.log(`${value}`)
+//   if (Number(value) > 0) {
+//     positive += 1
+//   } else if (Number(value) < 0) {
+//     negative += 1
+//   } else if (Number(value) === 0) {
+//     zero += 1
+//   }
+//   if (Number(value) % 2 === 0){
+//     even += 1
+//   } else if (Number(value) % 2 === 1){
+//     odd += 1
+//   }
+//     })
+// console.log(`Вы ввели ${positive} положительных чисел, ${negative} отрицательных, ${zero} нулей, из них ${even} четных и ${odd} нечетных`)
